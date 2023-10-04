903cb7e81ef3db569743bd8e088a3a11889cd241
Rajath Ramakrishna <rajath.ramakrishna@getcruise.com>
2023-10-03 21:26:44 -0700

    handle zero movies in sidebar

--- src/routes/sidebar.tsx
+++ src/routes/sidebar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { getMovies } from '../utils/movies';
-import { Link, NavLink, useLoaderData } from 'react-router-dom';
+import { NavLink, useLoaderData } from 'react-router-dom';
 import { Box } from '@chakra-ui/react';
 
 export function loader() {
@@ -10,9 +10,15 @@ export function loader() {
 export default function Sidebar() {
   const movies: any = useLoaderData();
   return (
-    <Box dropShadow={'lg'} color={'white'} p={4}>
-      {movies.map((movie: any) => (
-        <NavLink key={movie.id} to={`/movies/${movie.id}`}>
+    <Box dropShadow={'lg'} color={'white'} p={4} id='sidebar'>
+      { movies.length === 0 ?
+      <div>No Movies</div> :
+      movies.map((movie: any) => (
+        <NavLink
+        key={movie.id}
+        to={`/movies/${movie.id}`}
+        className={({ isActive }) => (isActive ? 'active' : '')}
+        >
           {movie.title}
         </NavLink>
       ))}
